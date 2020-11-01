e77986c2e3dcb129be77af2525267fb27014a0a3
Thatoddone <66497499+Thatoddone1@users.noreply.github.com>
2020-10-31 21:21:58 -0400

    added a way to read the passwords without opening the file

--- main.py
+++ main.py
@@ -3,14 +3,22 @@
 
 
 
+
+
+def seePasses():
+    f = open('passwords.txt', 'r')
+    passwords = f.read()
+    print(passwords)
+
+
 def addPassword():
-    webName = input('What would you like to name the username and password: ')
+    webName = input('What would you like to name the set: ')
     Username = input('What is the user name for the set: ')
     Password = input('What is the password for this set: ')
     f = open('passwords.txt', 'a')
-    f.write(webName + ': ' + Username + ': ' + Password)
+    f.write(webName + '; ' + Username + '; ' + Password + ',')
     f.write('\n')
     f.close()
 
 
-addPassword()
+seePasses()
